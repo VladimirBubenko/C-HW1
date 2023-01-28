51b16169d7ccbc9d09f64b6b1d85d0d6968695fb
Vladimir Bubenko <bawmc@mail.ru>
2023-01-28 16:40:47 +0700

    done task

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
-*/
+
 Console.Write("Введите число а: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число b: ");
@@ -23,3 +23,29 @@ else
     min = a;
 }
 Console.WriteLine($"max = {max} min = {min}");
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+Console.Write("max = " + max);
