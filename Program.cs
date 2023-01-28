80e3ce5696f865770b6bb1ca192d0e20244ec41c
Vladimir Bubenko <bawmc@mail.ru>
2023-01-28 16:27:44 +0700

    done task2

--- Program.cs
+++ Program.cs
@@ -5,4 +5,21 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
+Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
 
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine($"max = {max} min = {min}");
